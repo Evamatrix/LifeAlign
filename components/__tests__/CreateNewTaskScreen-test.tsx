15298964c68d7b3a6634911714162662c33c9cc4
Evamatrix <102815074+Evamatrix@users.noreply.github.com>
2024-10-27 23:05:11 -0500

    Add files via upload

--- components/__tests__/CreateNewTaskScreen-test.tsx
+++ components/__tests__/CreateNewTaskScreen-test.tsx
@@ -3,15 +3,35 @@ import { render, fireEvent } from '@testing-library/react-native';
 import CreateNewTaskScreen from '@/app/Screens/CreateNewTaskScreen';
 import { TaskProvider } from '@/app/src/context/TaskContext';
 
-describe('CreateNewTaskScreen', () => {
-    it('matches snapshot', () => {
-      const { toJSON } = render(
-        <TaskProvider>
-          <CreateNewTaskScreen />
-        </TaskProvider>
-      );
-  
-      // Create a snapshot of the CreateNewTaskScreen
-      expect(toJSON()).toMatchSnapshot();
-    });
-  });
+// Check for screen title rendering
+test('should render Create New Task screen title', () => {
+  const { getByText } = render(
+    <TaskProvider>
+      <CreateNewTaskScreen />
+    </TaskProvider>
+  );
+  expect(getByText('Create New Task')).toBeTruthy();
+});
+
+// Check for creation of new task
+test('should create a new task when form is submitted', () => {
+  const { getByPlaceholderText, getByText } = render(
+    <TaskProvider>
+      <CreateNewTaskScreen />
+    </TaskProvider>
+  );
+  fireEvent.changeText(getByPlaceholderText('Enter Title'), 'New Task');
+  fireEvent.press(getByText('Create Task'));
+
+  expect(getByText('New Task')).toBeTruthy();
+});
+
+// Check navigation
+test('should navigate back to Home Screen when back arrow is pressed', () => {
+  const { getByLabelText } = render(
+    <TaskProvider>
+      <CreateNewTaskScreen />
+    </TaskProvider>
+  );
+
+  fireEvent})
